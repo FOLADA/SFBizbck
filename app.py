ced175be242327a4ed6c20c7a8e5728a01528b34
FOLADA <128312088+FOLADA@users.noreply.github.com>
2025-07-14 15:14:43 +0400

    Update app.py
    lets try that

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from db import init_db
 from routes import bp as business_routes
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])  # ✅ Allow frontend
+CORS(app)
 
 init_db()
 
 app.register_blueprint(business_routes)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)  # Optional: specify port explicitly
+    app.run(debug=True)
